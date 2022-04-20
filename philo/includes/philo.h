de9c5ec0d9bc82e17bba8b4b0c4402151bf409da
Nathan FASCIA <nfascia@e1r3p13.42.fr>
2022-04-20 16:53:19 +0200

    coucou

---
+++ philo/includes/philo.h
@@ -0,0 +1,7 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <stdio.h>
+#include "ft_printf.h"
+
+#endif
---
+++ philo/srcs/main.c
@@ -0,0 +1,9 @@
+#include "philo.h"
+#include "ft_printf.h"
+#include "pthread.h"
+
+int main()
+{
+	ft_printf("coucou");
+	return (0);
+}
