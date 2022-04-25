fbfcc345651455c97d769c87dd47b68490026c91
Nathan FASCIA <nfascia@e3r6p16.clusters.42paris.fr>
2022-04-25 18:22:52 +0200

    thread mutex test

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -2,6 +2,15 @@
 # define PHILO_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "ft_printf.h"
+
+pthread_mutex_t lock;
+
+typedef struct  s_data
+{
+    int i;
+}
+t_data;
 
 #endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -2,8 +2,43 @@
 #include "ft_printf.h"
 #include "pthread.h"
 
+
+void	*ft_test(void *s)
+{
+	t_data *data;
+
+	data = (t_data *)s;
+	pthread_mutex_lock(&lock);
+	data->i++;
+	pthread_mutex_unlock(&lock);
+	return (NULL);
+}
+
 int main()
 {
-	ft_printf("coucou");
+	t_data *data;
+	data = malloc(sizeof(t_data));
+	data->i = 10;
+
+	pthread_t id1;
+	pthread_t id2;
+	pthread_t id3;
+
+
+	pthread_mutex_init(&lock, NULL);
+	usleep(2000);
+	pthread_create(&id1, NULL, ft_test, data);
+	pthread_create(&id2, NULL, ft_test, data);
+	pthread_create(&id3, NULL, ft_test, data);
+	
+	//pthread_mutex_lock(&lock);
+	ft_printf("%d\n", data->i);
+	//pthread_mutex_unlock(&lock);
+
+	pthread_join(id1, NULL);
+	pthread_join(id2, NULL);
+	pthread_join(id3, NULL);
+
+	pthread_mutex_destroy(&lock);
 	return (0);
 }
