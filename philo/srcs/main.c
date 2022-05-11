7cc3564974e76406dbffe94d6fd1d02a3ba9f225
Nathan FASCIA <nfascia@e2r12p2.clusters.42paris.fr>
2022-05-11 19:05:04 +0200

    coucou

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nfascia <nathanfascia@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 15:56:25 by nfascia           #+#    #+#             */
-/*   Updated: 2022/05/11 18:30:17 by nfascia          ###   ########.fr       */
+/*   Updated: 2022/05/11 19:03:10 by nfascia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,28 +29,41 @@
 // 	return (0);
 // }
 
-void	eat(t_thread **thread)
+int	philo_eat(t_thread **thread)
 {
-	if ((*thread)->philo_idx % 2 != 0)
-	{
-		if (pthread_mutex_lock(&(*thread)->philo->mutex_fork[(*thread)->philo_idx]) == 0)
-		philo_print(&(*thread)->philo, (*thread)->philo_idx, 1);
+	if (pthread_mutex_lock(&(*thread)->philo->mutex_fork[(*thread)->philo_idx]) == 0)
+	philo_print(&(*thread)->philo, (*thread)->philo_idx, 1);
 
-		if (pthread_mutex_lock(&(*thread)->philo->mutex_fork[(*thread)->philo_idx + 1]) == 0)
-		philo_print(&(*thread)->philo, (*thread)->philo_idx, 1);
-
-		philo_print(&(*thread)->philo, (*thread)->philo_idx, 2);
-			
-		pthread_mutex_unlock(&(*thread)->philo->mutex_fork[(*thread)->philo_idx]);
-		pthread_mutex_unlock(&(*thread)->philo->mutex_fork[(*thread)->philo_idx + 1]);
+	if (pthread_mutex_lock(&(*thread)->philo->mutex_fork[(*thread)->philo_idx + 1]) == 0)
+	philo_print(&(*thread)->philo, (*thread)->philo_idx, 1);
 		
-		(*thread)->last_meal = current_time() - (*thread)->philo->start_time;
-		if ((*thread)->last_meal >= (*thread)->philo->timetodie)
-		{
-			(*thread)->is_alive = 0;
-			philo_print(&(*thread)->philo, (*thread)->philo_idx, 5);
-		}
+	philo_print(&(*thread)->philo, (*thread)->philo_idx, 2);
+
+	usleep((*thread)->philo->timetoeat);
+
+	pthread_mutex_unlock(&(*thread)->philo->mutex_fork[(*thread)->philo_idx]);
+	pthread_mutex_unlock(&(*thread)->philo->mutex_fork[(*thread)->philo_idx + 1]);
+
+	(*thread)->last_meal = current_time() - (*thread)->philo->start_time;
+	(*thread)->eat_count = 1;
+
+	if ((*thread)->last_meal >= (*thread)->philo->timetodie)
+	{
+		(*thread)->is_alive = 0;
+		philo_print(&(*thread)->philo, (*thread)->philo_idx, 5);
 	}
+	return (0);
+}
+
+void	philo_think(t_thread **thread)
+{
+		philo_print(&(*thread)->philo, (*thread)->philo_idx, 4);
+}
+
+void	philo_sleep(t_thread **thread)
+{
+		philo_print(&(*thread)->philo, (*thread)->philo_idx, 3);
+		usleep((*thread)->philo->timetosleep);
 }
 
 
@@ -60,8 +73,14 @@ void	*routine(void *s)
 	thread = (t_thread *)s;
 	// (void)thread;
 	while (thread->is_alive == 1)
-		eat(&thread);
+	{
+		if (thread->philo_idx % 2 == 0)
+			philo_eat(&thread);
+		else if (thread->eat_count == 1)
+			philo_sleep(&thread);
 		
+			philo_think(&thread);
+	}		
 	return (NULL);
 }
 
