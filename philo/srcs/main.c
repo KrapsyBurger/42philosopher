f92fcf058eb342e6041fe7861f96c9ca28060991
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-05-16 01:35:44 +0200

    coucou

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,29 +6,28 @@
 /*   By: nfascia <nathanfascia@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 15:56:25 by nfascia           #+#    #+#             */
-/*   Updated: 2022/05/16 00:53:59 by nfascia          ###   ########.fr       */
+/*   Updated: 2022/05/16 01:31:36 by nfascia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
 int	philo_eat(t_thread *thread)
-{ 
+{
 	if (thread->philo_idx == 0)
 	{
-		if (pthread_mutex_lock(&thread->philo->mutex_fork[thread->philo_idx]) == 0)
+		if (pthread_mutex_lock(&thread->philo->mutex_fork[thread->philo->philonbr - 1]) == 0)
 			philo_print(&thread->philo, thread->philo_idx, 1);
-		if (pthread_mutex_lock(&thread->philo->mutex_fork[thread->philo_idx + 1]) == 0)
+		if (pthread_mutex_lock(&thread->philo->mutex_fork[thread->philo_idx]) == 0)
 			philo_print(&thread->philo, thread->philo_idx, 1);
 	}
 	else 
 	{
-		if (pthread_mutex_lock(&thread->philo->mutex_fork[thread->philo_idx - 1]) == 0)
-			philo_print(&thread->philo, thread->philo_idx, 1);
 		if (pthread_mutex_lock(&thread->philo->mutex_fork[thread->philo_idx]) == 0)
 			philo_print(&thread->philo, thread->philo_idx, 1);
+		if (pthread_mutex_lock(&thread->philo->mutex_fork[thread->philo_idx - 1]) == 0)
+			philo_print(&thread->philo, thread->philo_idx, 1);
 	}
-	// printf("\n\n%lld --------- %d\n\n", current_time() - thread->philo->start_time - thread->last_meal, thread->philo->timetodie); 
 
 	philo_print(&thread->philo, thread->philo_idx, 2);
 	thread->last_meal = current_time() - thread->philo->start_time;
@@ -37,13 +36,15 @@ int	philo_eat(t_thread *thread)
 	usleep(thread->philo->timetoeat * 1000);
 
 
-	pthread_mutex_unlock(&thread->philo->mutex_fork[thread->philo_idx - 1]);
-	pthread_mutex_unlock(&thread->philo->mutex_fork[thread->philo_idx]);
-
 	if (thread->philo_idx == 0)
 	{
+		pthread_mutex_unlock(&thread->philo->mutex_fork[thread->philo->philonbr - 1]);
 		pthread_mutex_unlock(&thread->philo->mutex_fork[thread->philo_idx]);
-		pthread_mutex_unlock(&thread->philo->mutex_fork[thread->philo_idx + 1]);
+	}
+	else
+	{
+		pthread_mutex_unlock(&thread->philo->mutex_fork[thread->philo_idx]);
+		pthread_mutex_unlock(&thread->philo->mutex_fork[thread->philo_idx - 1]);
 	}
 	return (0);
 }
@@ -68,24 +69,24 @@ void	*routine(void *s)
 	if (thread->philo->philonbr % 2 == 0)
 	{
 		if (thread->philo_idx % 2 != 0)
-			usleep(thread->philo->timetoeat * 1000);
+			usleep((thread->philo->timetoeat * 1000) / 2);
 	}
 	else if (thread->philo->philonbr % 2 != 0)
 	{
 		if (thread->philo_idx % 2 == 0)
-			usleep(thread->philo->timetoeat * 1000);
+			usleep((thread->philo->timetoeat * 1000) / 2);
 	}
 	while (thread->is_alive == 1)
 	{
+		philo_eat(thread);
+		philo_sleep(thread);
+		philo_print(&thread->philo, thread->philo_idx, 4);
 		if (current_time() - thread->philo->start_time - thread->last_meal >= thread->philo->timetodie)
 		{
 			philo_print(&thread->philo, thread->philo_idx, 5);
 			thread->is_alive = 0;
-			break ;
+			exit (1);
 		}
-		philo_eat(thread);
-		philo_sleep(thread);
-		philo_print(&thread->philo, thread->philo_idx, 4);
 		if (thread->philo->argc == 6)
 		{
 			if (thread->eat_count >= thread->philo->philomusteat)
